7df256c6807a01646db598af888fb2f93b0542d5
Jose Moreno <joseimoreno2013@gmail.com>
2018-02-13 17:40:01 -0800

    Assignment01
    JavaScript Essentials

---
+++ js/script.js
@@ -0,0 +1,161 @@
+/*es-lint browser*/
+// STEP1
+/*
+var someMonths;
+function theMonth;      // function to return current month
+currentMonth;           // a constant
+var summerMonth;        // an array of summer months
+myLibraryA_function     // a function
+*/
+
+// STEP 2
+/*
+3.9                     // numeric literal
+false                   // Boolean literal
+"Hello!"                // String literal
+null                    // literal null value
+*/
+
+// STEP 3
+/*
+var myValue = 2 + 2;
+var myValue2 = "This is when you go over the hill" + myValue * 10;
+*/
+
+// STEP 4
+/*
+var firstName;
+var lastName;
+var _address;
+var _city;
+var _state;
+var zipCode;
+var yourAge;
+var referralSource;
+var may_we_contact_you;
+*/
+
+// STEP 5
+/*
+var firstName = "Jose";
+var lastName = "Moreno";
+var _address = "6555 Mallard street", _city = "San Diego", _state = "CA", zipCode = 92114;
+var yourAge = 43;
+var referralSource = "Word of mouth";
+var may_we_contact_you = true;
+*/
+
+// STEP 6
+/*
+var myNumber = 11;
+window.console.log("This is my number " + myNumber + "!");
+*/
+/*
+var myProfession = "Graphic Designer";
+var isProfession = true;
+window.console.log("Is Jose a " + myProfession + " ? " + isProfession);
+*/
+/*
+var myNumber = 2 + 6;
+var theAnswer = 9;
+var wrongAnswer = false;
+window.console.log("Is " + myNumber + " equal to " + theAnswer + " ? " + wrongAnswer);
+*/
+
+// STEP 7
+/*
+var make = "Scion";
+window.console.log("The car is a " + make + " " + model + ".");
+var model = "XB";
+*/
+
+// STEP 8
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);
+*/
+
+// STEP 9
+/*
+var x = 400;
+var y = null;
+x = null;
+window.console.log(x);
+window.console.log(y);
+window.console.log(z);
+var z = x + y;
+*/
+
+// STEP 10
+/*
+typeof "Jose is writing js"
+typeof 1.11
+typeof false
+typeof [1,2,3,4]
+typeof myCar
+*/
+
+// STEP 11
+/*
+window.alert("Hello " + "Jose Moreno, welcome to the JavaScript class!");
+*/
+
+// STEP 12
+/*
+var myName = "Jose Moreno";
+window.alert("Hello " + myName + ", welcome to the JavaScript class!");
+*/
+
+//STEP 13
+/*
+var myName = "Jose Moreno";
+var course = "JavaScript";
+window.alert("Hello " + myName + ", welcome to the " + course + " class!");
+*/
+
+// STEP 14
+/*
+var myName = "Jose Moreno";
+var course = "JavaScript";
+window.alert("Hello " + myName + ",\n welcome to the " + course + " class!");
+*/
+
+// STEP 15
+/*
+var myName = "";
+window.prompt("What is your name?");
+*/
+
+// STEP 16
+/*
+var course = "";
+window.prompt("What course are you taking?");
+*/
+
+// STEP 17
+/*
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+*/
+
+// STEP 18
+/*
+var x = 20;
+window.console.log(x + 20);
+*/
+// STEP 19
+/*
+var x = 20;
+window.console.log(x * 5);
+*/
+
+// STEP 20
+
+var x = 20;
+x /= 3;
+window.console.log(x += 1);
+
+
+
+
